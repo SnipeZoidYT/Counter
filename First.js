061919ef0268daa57bd0963b430de39c6a2cf6ad
Christopher Todd <98193686+SnipeZoidYT@users.noreply.github.com>
2024-07-15 21:27:53 +0100

    Added all files
    Added all the files for the counter to work.

---
+++ First.js
@@ -0,0 +1,8 @@
+const userName = prompt('Please enter your name.');
+if (userName !== null) {
+    alert ('Hello ' + userName)
+}
+const userAge = prompt('How old are you ' + userName + '.');
+if (userAge !== null) {
+    alert (userAge + ' nice i am 205 years old.')
+}
